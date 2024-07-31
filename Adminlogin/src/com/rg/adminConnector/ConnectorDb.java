bac0644391790d4646ebc114f5702f093421cc3e
GokulKrishna0931 <132735462+Gokulkrishana@users.noreply.github.com>
2024-07-31 19:49:00 +0530

    Update ConnectorDb.java

--- Adminlogin/src/com/rg/adminConnector/ConnectorDb.java
+++ Adminlogin/src/com/rg/adminConnector/ConnectorDb.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class ConnectorDb {
 	static String url = "jdbc:mysql://localhost:3306/adminblock";
 	static String uname = "root";
-	static String pwd = "gokul";
+	static String pwd = "";
 	static Connection con = null;
 	public static Connection requestCon() throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
